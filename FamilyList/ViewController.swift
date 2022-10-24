783be114dde7c95b8eeb6482606d3befd7dd370b
zalik198 <zshomakhov@mail.ru>
2022-10-24 23:05:36 +0300

    Initial Commit

---
+++ FamilyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamilyList
+//
+//  Created by Shom on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
